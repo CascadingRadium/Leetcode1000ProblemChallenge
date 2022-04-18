c1d3f7e617c8793162129e3220ec0c4722023aba
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-18 14:51:39 +0530

    Time: 29 ms (22.22%), Space: 14.7 MB (14.38%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) 
+    {
+        int maxl=max(num1.length(),num2.length());
+        if(num1.length()==maxl)
+        {
+            int l=maxl-num2.length();
+            for(int i=0;i<l;i++)
+                num2="0"+num2;
+        }
+        else
+        {
+            int l=maxl-num1.length();
+            for(int i=0;i<l;i++)
+                num1="0"+num1;
+        }
+        int mult=0;
+        int carry=0;
+        vector<vector<int>> matrix(maxl,vector<int>(2*maxl));
+        int row=0,col=(2*maxl)-1,initcol=col;
+        for(int i=maxl-1;i>=0;i--)
+        {
+            for(int j=maxl-1;j>=0;j--)
+            {
+                int x=(num1[j]-'0')*(num2[i]-'0')+carry;
+                matrix[row][col]=x%10;
+                carry=x/10;
+                col--;
+            }
+            matrix[row][col]=carry;
+            row++;
+            col=initcol-1;
+            initcol=col;
+            carry=0;
+        }
+        carry=0;
+        string ret="";
+        for(int i=2*maxl-1;i>=0;i--)
+        {
+            mult=0;
+            mult+=carry;
+            for(int j=0;j<maxl;j++)
+            {
+                mult+=matrix[j][i];
+            }
+            ret=to_string(mult%10)+ret;
+            carry=mult/10;
+        }
+        while(ret[0]=='0')
+            ret.begin()=ret.erase(ret.begin());
+        if(ret=="")
+            return "0";
+        if(carry)
+            return "1"+ret;
+        return ret;
+        
+    }
+};
