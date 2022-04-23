4f9dc7ccba0f2d9fdba1d293eb5d186d6a100f37
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 20:39:47 +0530

    Time: 8 ms (71.92%), Space: 6.6 MB (57.92%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -2,31 +2,29 @@ class Solution {
 public:
     string countAndSay(int n) 
     {
-            string x="1";
-    while(n>1)
-    {
-        --n;
-        string y="";
-        char firstchar=x[0];
-        int fcharcnt=0;
-        for(auto i:x)
+        string x="1";
+        while(n>1)
         {
-            if(i==firstchar)
-                fcharcnt++;
-            else
+            --n;
+            string y="";
+            char firstchar=x[0];
+            int fcharcnt=0;
+            for(auto i:x)
             {
-                y+=to_string(fcharcnt);
-                y.push_back(firstchar);
-                firstchar=i;
-                fcharcnt=1;
+                if(i==firstchar)
+                    fcharcnt++;
+                else
+                {
+                    y+=to_string(fcharcnt);
+                    y.push_back(firstchar);
+                    firstchar=i;
+                    fcharcnt=1;
+                }
             }
+            y+=to_string(fcharcnt);
+            y.push_back(firstchar);
+            x=y;
         }
-        y+=to_string(fcharcnt);
-        y.push_back(firstchar);
-        x=y;
-    }
-    return x;
-        
-        
+        return x;
     }
 };
