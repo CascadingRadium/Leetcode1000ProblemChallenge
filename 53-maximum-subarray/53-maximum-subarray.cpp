8fefe6db1dda811270d719a99bcaf715dd5a1497
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 23:20:59 +0530

    Time: 265 ms (5.12%), Space: 67.7 MB (91.56%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) 
-    {
-        int max=INT_MIN;
-        int tempmax=0;
-        for(int i=0;i<nums.size();++i)
-        {
-            tempmax=std::max(nums[i],tempmax+nums[i]);
-            max=std::max(max,tempmax);
-        }
-        return max;
+    int maxSubArray(vector<int>& nums) {
+        int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
     }
 };
