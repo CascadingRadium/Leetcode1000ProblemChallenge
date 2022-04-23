2367adfaadefe37911e9d242022f0c2b97a97f21
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 22:27:43 +0530

    Time: 37 ms (33.60%), Space: 20.3 MB (27.83%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        vector<set<char>> hmaparray(27);
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                char n=board[i][j];
+                if(board[i][j]!='.')
+                {
+                    if(hmaparray[i].count(n)>0||hmaparray[9+j].count(n)>0||hmaparray[18+(3*(i/3))+(j/3)].count(n)>0)
+                        return false;
+                    hmaparray[i].insert(n);
+                    hmaparray[9+j].insert(n);
+                    hmaparray[18+(3*(i/3))+(j/3)].insert(n);
+                        
+                }
+            }
+        }
+
+        return true;
+    }
+};
