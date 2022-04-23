b3c0c7dbb6d5aabb4d52fc14514fb6fa69f52ef6
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 18:22:55 +0530

    Time: 4 ms (77.67%), Space: 6.8 MB (38.93%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -5,26 +5,29 @@ public:
         int maxl=max(num1.length(),num2.length());
         int l1=num1.length();
         int l2=num2.length();
+        reverse(num1.begin(),num1.end());
+        reverse(num2.begin(),num2.end());
         if(l1<maxl)
         {
             for(int i=0;i<maxl-l1;i++)
-                num1='0'+num1;
+                num1+='0';
         }
         else
         {
             for(int i=0;i<maxl-l2;i++)
-                num2='0'+num2;
+                num2+='0';
         }
         bool carry=0;
         string x="";
-        for(int i=maxl-1;i>=0;i--)
+        for(int i=0;i<maxl;i++)
         {
             int y=((num1[i]-'0')+(num2[i]-'0'))+carry;
-            x=(to_string(y%10))+x;
+            x+=(to_string(y%10));
             carry=y/10;
         }
         if(carry)
-            x="1"+x;
+            x+="1";
+        reverse(x.begin(),x.end());
         return x;
         
     }
