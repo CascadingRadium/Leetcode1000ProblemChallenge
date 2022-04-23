5826a46d763a98af2f88439820fbabe547ccca2d
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 22:32:32 +0530

    Time: 24 ms (76.75%), Space: 18.2 MB (66.26%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        vector<bitset<9>> hmaparray(27);
+        vector<bitset<9>> bitsetarray(27);
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
@@ -10,16 +10,15 @@ public:
                 char n=board[i][j];
                 if(n!='.')
                 {
-                    if(hmaparray[i][n]>0||hmaparray[9+j][n]>0||hmaparray[18+(3*(i/3))+(j/3)][n]>0)
+                    if(bitsetarray[i][n]>0||bitsetarray[9+j][n]>0||bitsetarray[18+(3*(i/3))+(j/3)][n]>0)
                         return false;
-                    hmaparray[i][n]=true;
-                    hmaparray[9+j][n]=true;
-                    hmaparray[18+(3*(i/3))+(j/3)][n]=true;
+                    bitsetarray[i][n]=true;
+                    bitsetarray[9+j][n]=true;
+                    bitsetarray[18+(3*(i/3))+(j/3)][n]=true;
                         
                 }
             }
         }
-
         return true;
     }
 };
