6e3c2c4c09593911e55d9e9ca2fa3bbdc1cbe9fe
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-24 02:45:41 +0530

    Time: 69 ms (35.89%), Space: 30.9 MB (95.68%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    bool dfs(vector<int>&nums,int k)
+    bool canReach(vector<int>& nums, int k)
     {
         if(k<0||k>=nums.size()||nums[k]<0)
             return false;
         if(nums[k]==0)
             return true;
         nums[k]*=-1;
-        if(dfs(nums,k+nums[k]) || dfs(nums,k-nums[k]))
+        if(canReach(nums,k+nums[k]) || canReach(nums,k-nums[k]))
             return true;
         return false;  
-        
-    }
-    bool canReach(vector<int>& nums, int k)
-    {
-        return dfs(nums,k);
     }
 };
