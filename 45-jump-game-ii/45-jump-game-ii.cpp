8204c60eea62c92d59606900f8b605273f5e9a58
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 16:56:32 +0530

    Time: 303 ms (35.61%), Space: 17.2 MB (35.66%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        vector<int> row(nums.size());
+        int n=nums.size();
+        row[n-1]=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            int minjmp=INT_MAX;
+            for(int jmp=1;jmp<=nums[i];jmp++)
+            {
+                if(i+jmp<n)
+                    minjmp=min(row[i+jmp],minjmp);
+            }
+            minjmp==INT_MAX?row[i]=INT_MAX:row[i]=minjmp+1;
+        }
+        return row[0];
+    }
+};
