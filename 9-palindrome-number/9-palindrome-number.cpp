7ae80b91ffd437e6b4cd243ed1f0fcdcd8fb9480
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 19:33:21 +0530

    Time: 11 ms (83.83%), Space: 5.7 MB (91.74%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     bool isPalindrome(int x) 
     {
+        if(x<0)
+            return false;
         long long y=0;
         int control=x;
-        while(x>0)
+        while(x!=0)
         {
             int r=x%10;
             y=y*10+r;
