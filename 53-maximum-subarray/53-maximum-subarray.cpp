2396f15d157153c52a6741fa84ff4e37d3c06d53
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 23:25:58 +0530

    Time: 136 ms (57.68%), Space: 67.9 MB (12.32%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,7 +4,8 @@ public:
     {
         int maxD=nums[0];
         int tempmax=nums[0];
-        for(int i=1;i<nums.size();++i)
+        int n=nums.size();
+        for(int i=1;i<n;++i)
         {
             tempmax=max({nums[i],tempmax+nums[i]});
             maxD=max(maxD,tempmax);
