5319fe16b428977cc3620fb0d3f4bfada5dc927c
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-23 23:25:17 +0530

    Time: 176 ms (22.90%), Space: 67.8 MB (12.32%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) 
     {
-        int max=nums[0];
+        int maxD=nums[0];
         int tempmax=nums[0];
         for(int i=1;i<nums.size();++i)
         {
-            tempmax=std::max({nums[i],tempmax+nums[i]});
-            max=std::max(max,tempmax);
+            tempmax=max({nums[i],tempmax+nums[i]});
+            maxD=max(maxD,tempmax);
         }
-        return max;
+        return maxD;
     }
 };
