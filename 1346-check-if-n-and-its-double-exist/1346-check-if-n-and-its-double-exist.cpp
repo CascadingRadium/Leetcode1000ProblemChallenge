910d811fe9361f1f4806bbaafd334acfff5a4fc3
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-24 13:22:50 +0530

    Time: 4 ms (91.91%), Space: 10.3 MB (36.34%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -3,14 +3,6 @@ public:
     bool checkIfExist(vector<int>& arr) 
     {
         unordered_set<int> s1;
-        int zerocnt=0;
-        for(auto i : arr)
-        {
-            if(i==0)
-                zerocnt++;
-        }
-        if(zerocnt>1)
-            return true;
         for(auto i:arr)
         {
             if(s1.count(2*i)||(i%2==0&&s1.count(i/2)))
