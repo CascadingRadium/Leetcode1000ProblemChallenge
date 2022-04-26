32fa17ac640e3880543067fb999b5ec36422ddd7
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-26 22:38:20 +0530

    Time: 162 ms (55.85%), Space: 93.4 MB (53.92%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -6,9 +6,11 @@ public:
         int curprof=0;
         for(int i=1;i<prices.size();i++)
         {
-            if(prices[i]-min>curprof)
-                curprof=prices[i]-min;
-            min=std::min(min,prices[i]);
+            int t=prices[i]-min;
+            if(t>curprof)
+                curprof=t;
+            if(min>prices[i])
+                min=prices[i];
         }
         return curprof;
         
