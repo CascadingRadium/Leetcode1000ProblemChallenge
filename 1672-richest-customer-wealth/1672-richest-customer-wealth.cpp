860a612512f6b4765ba2d9cc714f0044d9549872
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-04-30 20:26:06 +0530

    Time: 3 ms (95.31%), Space: 8.1 MB (9.26%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) 
+    {
+        int ret=0;
+        for(auto i:accounts)
+        {
+            int sum=0;
+            for(auto j:i)
+            {
+                sum+=j;
+            }
+            ret=max(ret,sum);
+        }
+        return ret;
+    }
+};
