1d7c082231199b31c76b96a33a068148973723ca
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-05-01 17:58:14 +0530

    Time: 17 ms (33.70%), Space: 7.3 MB (47.23%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        unordered_map<char,int> hmap;
+        for(auto i:s)
+            hmap[i]++;
+        for(auto i:t)
+            hmap[i]--;
+        for(auto i:hmap)
+            if(i.second!=0)
+                return false;
+        return true;
         
     }
 };
