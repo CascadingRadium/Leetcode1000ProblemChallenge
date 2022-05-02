f680ef1ca64cb39054cb7391294e3c7fe7f24039
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-05-02 10:08:06 +0530

    Time: 8 ms (18.47%), Space: 6.8 MB (25.99%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-        unordered_map<char,int> hmap;
-        for(auto i:s)
-            hmap[i]++;
-        for(auto i:t)
-            hmap[i]--;
-        char ret;
-        for(auto i:hmap)
-            if(i.second!=0)
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        char ret='#';
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!=t[i])
             {
-                ret=i.first;
+                ret=t[i];
                 break;
             }
+        }
+        if(ret=='#')
+            ret=t[s.length()];
         return ret;
     }
 };
