fa6f844803b2f7e42a900c2f06686291c389cf67
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-05-02 10:00:42 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (15.75%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string freqAlphabets(string s) 
+    {
+        unordered_map<string,char> hmap;
+        for(int i=0;i<10;i++)
+            hmap[to_string(i+1)]='a'+i;
+        for(int i=10;i<27;i++)
+            hmap[to_string(i)+"#"]='a'+(i-1);
+        string ret="";
+        int i=0;
+        for(i=0;i<s.length()-2;i++)
+        {
+            if(s[i+2]=='#')
+            {
+                ret.push_back(hmap[s.substr(i,3)]);
+                i+=2;
+                
+            }
+            else
+                ret.push_back(hmap[string(1,s[i])]);
+        }
+        if(i!=s.length())
+        {
+            ret.push_back(hmap[string(1,s[i++])]);
+        }
+        if(i!=s.length())
+        {
+            ret.push_back(hmap[string(1,s[i])]);
+        }
+        return ret;
+        
+        
+    }
+};
