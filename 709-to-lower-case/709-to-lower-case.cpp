0f452ed4fbdf0f9304d389ebd08b1bc601bb4a51
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-05-02 09:31:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (7.82%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -3,10 +3,7 @@ public:
     string toLowerCase(string s) 
     {
         for(int i=0;i<s.length();i++)
-        {
-
-                s[i]=tolower(s[i]);
-        }
+            s[i]=tolower(s[i]);
         return s;
     }
 };
