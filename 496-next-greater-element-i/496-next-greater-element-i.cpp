f8022f71ea28ddebd3b19aedba751e68388f96f8
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-05-02 23:47:01 +0530

    Time: 10 ms (48.46%), Space: 8.9 MB (29.93%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -9,16 +9,10 @@ public:
             while(!st.empty()&&st.top()<nums2[i])
                 st.pop();
             if(st.empty())
-            {
                 hmap[nums2[i]]=-1;
-                st.push(nums2[i]);
-            }
             else
-            {
                 hmap[nums2[i]]=st.top();
-                st.push(nums2[i]);
-            }
-                
+            st.push(nums2[i]);    
         }
         vector<int> ret;
         for(auto i:nums1)
