304e9da3cc7992398b2725170d27752e7cfb0029
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-05-04 20:31:18 +0530

    Time: 663 ms (5.01%), Space: 89.4 MB (7.04%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         multimap<int,int> hmap;
-        vector<int> visited(nums.size());
         int ret=0;
         for(int i=0;i<nums.size();i++)
         {
@@ -14,10 +13,6 @@ public:
             else    
                 hmap.insert(make_pair(nums[i],i));
         }
-        for(auto i:hmap)
-            cout<<i.first<<' '<<i.second<<'\n';
         return ret;
-        
-        
     }
 };
