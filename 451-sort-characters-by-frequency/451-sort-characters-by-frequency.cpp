90e45795fd9e21c2430a51a22e723e45b3274fe0
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-08-14 05:54:32 +0530

    Time: 205 ms (5.01%), Space: 8.1 MB (88.02%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -5,15 +5,12 @@ public:
         unordered_map<char,int> hmap;
         for(auto chr:s)
             hmap[chr]++;
-        string x="";
-        vector<pair<char,int>> hm(hmap.begin(),hmap.end());
-        sort(hm.begin(),hm.end(),[](auto a, auto b){return a.second>b.second;});
-        for(auto i:hm)
-        {
-            for(int j=0;j<i.second;j++)
-                x.push_back(i.first);
-        }
-        return x;
+        sort(s.begin(),s.end(),[&hmap](char a, char b){
+            if(hmap[a]==hmap[b])
+                return a>b;
+            return hmap[a]>hmap[b];
+        });
+        return s;
         
         
     }
