76ffa3ed9a12b3f0c7adc5b6cf4313c87477f044
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-08-14 08:03:43 +0530

    Time: 194 ms (98.49%), Space: 57.1 MB (89.30%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,33 +1,10 @@
 class Solution {
 public:
-    struct comp
-    {
-        bool operator()(pair<vector<int>,double>& a, pair<vector<int>,double>& b)
-        {
-            return a.second<b.second;
-        }
-    };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
     {
-        double distance=0;
-        priority_queue<pair<vector<int>,double>,vector<pair<vector<int>,double>>,comp> pq;
-        for(auto i:points)
-        {
-            distance=sqrt(pow(i[0],2)+pow(i[1],2));
-            if(pq.size()<k)
-                pq.push({i,distance});
-            else
-            {
-                pq.push({i,distance});
-                pq.pop();
-            }
-        }
-        vector<vector<int>> ret;
-        while(!pq.empty())
-        {
-            ret.push_back(pq.top().first);
-            pq.pop();
-        }
-        return ret;
+        nth_element(points.begin(), points.begin() + k , points.end(), [](vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        });
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
