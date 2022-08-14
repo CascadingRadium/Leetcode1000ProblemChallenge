0cd1acfb25f014400f945fbb4f86e9964f7598f4
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-08-14 06:42:51 +0530

    Time: 437 ms (42.01%), Space: 79.5 MB (21.02%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+    struct comp
+    {
+        bool operator()(pair<vector<int>,double>& a, pair<vector<int>,double>& b)
+        {
+            return a.second<b.second;
+        }
+    };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
     {
         double distance=0;
-        vector<pair<vector<int>,double>> hmap;
+        priority_queue<pair<vector<int>,double>,vector<pair<vector<int>,double>>,comp> pq;
         for(auto i:points)
         {
             distance=sqrt(pow(i[0],2)+pow(i[1],2));
-            hmap.push_back({i,distance});
+            if(pq.size()<k)
+                pq.push({i,distance});
+            else
+            {
+                pq.push({i,distance});
+                pq.pop();
+            }
         }
-        sort(hmap.begin(),hmap.end(),[](auto &a,auto&b){return a.second<b.second;});
         vector<vector<int>> ret;
-        for(int i=0;i<k;i++)
+        while(!pq.empty())
         {
-            ret.push_back(hmap[i].first);
+            ret.push_back(pq.top().first);
+            pq.pop();
         }
         return ret;
     }
