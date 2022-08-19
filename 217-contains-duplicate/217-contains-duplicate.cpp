3b87f0d089bec2c587d2403f7f61e464c806693c
Rahul Sanjay Rampure <rampurerahul0@gmail.com>
2022-08-19 10:08:51 +0530

    Time: 120 ms (76.23%), Space: 46.6 MB (92.05%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) 
     {
-        ;
-        return unordered_set (nums.begin(),nums.end()).size()!=nums.size();
-        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+                return true;
+        }
+        return false;
     }
 };
